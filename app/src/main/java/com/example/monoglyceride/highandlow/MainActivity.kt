57608fd99e1e11dc6250e7a5a0b13f1fe71e9b3e
mono-glyceride <glyceride.mono@gmail.com>
2023-10-12 01:20:53 +0900

    update 変数を追加

--- app/src/main/java/com/example/monoglyceride/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/monoglyceride/highandlow/MainActivity.kt
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    private val tag = "high and low"
+    private var yourCard = 0
+    private var droidCard = 0
+    private var hitCount = 0
+    private var missCount = 0
+    private var gameStart = false
+    private var answerd = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
