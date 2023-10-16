9a45037388b82daab0fc9195d127407a520abfc8
mono-glyceride <glyceride.mono@gmail.com>
2023-10-17 07:56:37 +0900

    add onResume drawCard

--- app/src/main/java/com/example/monoglyceride/highandlow/MainActivity.kt
+++ app/src/main/java/com/example/monoglyceride/highandlow/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.monoglyceride.highandlow
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.monoglyceride.highandlow.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,5 +19,42 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hitCount = 0
+        missCount = 0
+        binding.hitText.text = getString(R.string.hit_text)
+        binding.missText.text = getString(R.string.lose_text)
+        gameStart = true
+        drawCard()
+    }
+
+    private fun drawCard() {
+        binding.youtCardImage.setImageResource(R.drawable.trannpu)
+        binding.droidCardImage.setImageResource(R.drawable.trannpu_black)
+
+        // IntRange.random()で乱数を生成
+        yourCard = (1..13).random()
+        Log.d(tag, "You:" + yourCard)
+        when(yourCard){
+            1 -> binding.youtCardImage.setImageResource(R.drawable.d1)
+            2 -> binding.youtCardImage.setImageResource(R.drawable.d2)
+            3 -> binding.youtCardImage.setImageResource(R.drawable.d3)
+            4 -> binding.youtCardImage.setImageResource(R.drawable.d4)
+            5 -> binding.youtCardImage.setImageResource(R.drawable.d5)
+            6 -> binding.youtCardImage.setImageResource(R.drawable.d6)
+            7 -> binding.youtCardImage.setImageResource(R.drawable.d7)
+            8 -> binding.youtCardImage.setImageResource(R.drawable.d8)
+            9 -> binding.youtCardImage.setImageResource(R.drawable.d9)
+            10 -> binding.youtCardImage.setImageResource(R.drawable.d10)
+            11 -> binding.youtCardImage.setImageResource(R.drawable.d11)
+            12 -> binding.youtCardImage.setImageResource(R.drawable.d12)
+            13 -> binding.youtCardImage.setImageResource(R.drawable.d13)
+        }
+        droidCard = (1..13).random()
+        Log.d(tag, "droid:"+droidCard)
+        answerd = false
     }
 }
